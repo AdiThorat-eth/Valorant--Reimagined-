a1ebda859abc8cf0e92335d0b2abd53c17936e44
Aditya Thorat <a.workspace.00@gmail.com>
2025-07-22 18:38:31 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Button({title, id, rightIcon, leftIcon, containerClass}) {
+function Button({ title, id, rightIcon, leftIcon, containerClass, onClick }) {
   return (
-    <button id = {id} 
-    className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black flex transition-all duration-300 hover:bg-violet-500 ${containerClass}`}
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black flex transition-all duration-300 hover:bg-violet-500 ${containerClass}`}
+      onClick={onClick}
     >
       {leftIcon}
       <span className="relative flex overflow-hidden font-General text-xs uppercase">
-        <div>
-          {title}
-        </div>
+        <div>{title}</div>
       </span>
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
